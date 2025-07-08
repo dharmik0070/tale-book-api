f1cdee5e5d031a35b47546eeaf4644c551cde56d
dharmik0070 <dharmik0070@gmail.com>
2025-07-08 10:26:57 +0530

    [Unit Tests] Added tests

---
+++ Talebook.Tests/NewsServiceTests.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Caching.Memory;
+using Moq;
+using Moq.Protected;
+using System.Net;
+using System.Net.Http.Json;
+using Talebook.Infrastructure.Services;
+
+public class NewsServiceTests
+{
+    private readonly IMemoryCache _memoryCache;
+
+    public NewsServiceTests()
+    {
+        _memoryCache = new MemoryCache(new MemoryCacheOptions());
+    }
+
+    [Fact]
+    public async Task GetTopTalesAsync_ReturnsListOfTales()
+    {
+        // Arrange
+        var storyIds = new List<int> { 1, 2 };
+        var response1 = new { title = "Title 1", url = "http://url1.com" };
+        var response2 = new { title = "Title 2", url = "http://url2.com" };
+
+        var handlerMock = new Mock<HttpMessageHandler>();
+
+        handlerMock
+            .Protected()
+            .SetupSequence<Task<HttpResponseMessage>>("SendAsync",
+                ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(new HttpResponseMessage { StatusCode = HttpStatusCode.OK, Content = JsonContent.Create(storyIds) })
+            .ReturnsAsync(new HttpResponseMessage { StatusCode = HttpStatusCode.OK, Content = JsonContent.Create(response1) })
+            .ReturnsAsync(new HttpResponseMessage { StatusCode = HttpStatusCode.OK, Content = JsonContent.Create(response2) });
+
+        var httpClient = new HttpClient(handlerMock.Object)
+        {
+            BaseAddress = new Uri("https://hacker-news.firebaseio.com/")
+        };
+
+        var service = new NewsService(httpClient, _memoryCache);
+
+        // Act
+        var result = await service.GetTopTalesAsync(2);
+
+        // Assert
+        result.Should().HaveCount(2);
+        result[0].Title.Should().Be("Title 1");
+        result[1].Url.Should().Be("http://url2.com");
+    }
+}
