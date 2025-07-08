2907e1a95fa71dbf6f4ef0ac94636f0fab43a357
dharmik0070 <dharmik0070@gmail.com>
2025-07-08 10:25:04 +0530

    Added entry point file

---
+++ Talebook.WebApi/Program.cs
@@ -0,0 +1,32 @@
+using Talebook.Application.Interfaces;
+using Talebook.Infrastructure.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddMemoryCache();
+builder.Services.AddHttpClient<INewsService, NewsService>();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+app.UseCors("AllowAll");
+
+// Swagger
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
